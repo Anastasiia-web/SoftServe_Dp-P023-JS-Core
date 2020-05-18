76b343ec17fea7196d9ef77fcb776f582a2e8d8f
Anastasiia-web <anastasiyamarket@gmail.com>
2020-05-19 00:26:32 +0300

    update

--- mini_tasks/color_palette/color_palette.js
+++ mini_tasks/color_palette/color_palette.js
@@ -6,3 +6,39 @@ fifth_square.onclick = function() {
     document.body.style.backgroundColor = change_page_color();  
 }
 */
+
+/*функция изменения цвета выпадающего списка
+document.querySelectorAll('#fifth_square').forEach(function(element){
+    element.onclick = bgChange;
+});
+
+// let timer = 0;
+
+// function bgChange(){
+// 	let btnlist = document.querySelectorAll('.btn__list');
+// 	timer++;
+//     if(timer%2 == 0){
+//     	for(let i = 0; i < btnlist.length; i++){
+    		
+//     	        btnlist.style.color = '#ffffff';
+    		
+//         }
+//     }
+//     else if(timer%2 != 0){
+//         for(let i = 0; i < btnlist.length; i++){
+    
+//     	        btnlist[i].style.background = '#ffcc00';
+            
+//         }
+
+//         let change = document.querySelectorAll('.btn__list p');
+//   	    for(let i = 0; i < change.length; i++){
+  	    	
+// 		        change[i].style.color = '#ba0000';
+	        
+// 	    }
+//     }
+
+//     document.querySelector('.red').style.maxHeight = 'none';
+// 	document.querySelector('.red').style.minHeight = '340px';
+// }
