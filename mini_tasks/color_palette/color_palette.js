2025b876845f180c30bf861608ad2ec297e51e53
Anastasiia-web <anastasiyamarket@gmail.com>
2020-05-18 22:35:08 +0300

    update

---
+++ mini_tasks/color_palette/color_palette.js
@@ -0,0 +1,8 @@
+/*function change_page_color() {
+    document.body.style.backgroundColor="lavender";
+}
+ 
+fifth_square.onclick = function() {
+    document.body.style.backgroundColor = change_page_color();  
+}
+*/
