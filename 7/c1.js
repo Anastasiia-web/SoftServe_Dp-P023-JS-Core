c34189335c8918fbd5844c01b1b449fe9a4d0e73
Anastasiia-web <anastasiyamarket@gmail.com>
2020-05-29 04:12:36 +0300

    update

--- 7/c1.js
+++ 7/c1.js
@@ -1,4 +1,4 @@
-let a = document.getElementsByTagName("userInput");
+let mathCalculator = function() {
 const A = randNumber();
 const B = randNumber();
 const C = randOperand();
@@ -15,71 +15,103 @@ function randOperand(min = 1, max = 4) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-if(C == 1){
-    given.innerHTML = `${A} + ${B}`;
-} else if(C == 2){
-    given.innerHTML = `${A} - ${B}`;
-} else if(C == 3){
-    given.innerHTML = `${A} * ${B}`;
-} else if(C == 4){
-    given.innerHTML = `${A} / ${B}`;
+const start = function() {
+    if(C == 1){
+        given.innerHTML = `${A} + ${B}`;
+    } else if(C == 2){
+        given.innerHTML = `${A} - ${B}`;
+    } else if(C == 3){
+        given.innerHTML = `${A} * ${B}`;
+    } else if(C == 4){
+        given.innerHTML = `${A} / ${B}`;
+    }
 }
 
-function count(value) {  
-    let g = value;
+start();
+
+function count() {  
+    g = w.value;
     if(g == optionS || g == optionM || g == optionMul || g == optionD) {
         result.innerHTML = 'Well done!';
     } else {
         result.innerHTML = 'Try again!';
+        //w.value = '';                    //через айди.value очищается поле ввода
     }  
 }
+
 submit.onclick = count;
 
+w.value = '';
 
-
-
-/* 1 вариант
-function new_task(value) {  
-    a.innertext = '';
 }
 
-more.onclick = new_task;
-*/
-/*
-
-let more_but = document.querySelector('#more');
-
-more_but.addEventListener("click", start);
-
-*/
+more.onclick = mathCalculator;
+setInterval ( mathCalculator, 5000 );
 
 
+let limit = mathCalculator("limit")||5;
+limit = parseInt(limit);
 
+processTimer();
 
-
-
-
-//  не получилось очистить
-/*
-more.onclick = function () {
-    a.innerText = '';
-}
-*/
-
-/*
-function moreExe() {
-    a.value = '';
+function processTimer() {
+  if (limit > 0) {
+    setTimeout("processTimer()", 1000);
+    limit--;
+  } else {
+    timer.style.color = `rgb(255, 0, 0)`;
+    timer.style.fontSize = '25px';
+  }
+  let limit_div = ("0"+parseInt(limit / 60)).slice(-2); // минуты
+  let limit_mod = ("0"+(limit - limit_div * 60)).slice(-2); // секунды
+  // вывод времени
+  el_timer = document.getElementById("timer");
+  if (el_timer) el_timer.innerHTML = limit_div+":"+limit_mod;
 }
 
-moreExe.onclick = moreExe;
+
+/* //конспект работающий таймер
+
+function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((t / 1000) % 60);
+    
+    return {
+      total: t,
+      seconds: seconds
+    };
+  }
+   
+  function initializeClock(id, endtime) {
+    let clock = document.getElementById(id);
+    let secondsSpan = clock.querySelector(".seconds");
+   
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+   
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+        let deadline = new Date(Date.parse(new Date()) + 5 * 1000);
+        initializeClock('countdown', deadline);
+      }
+      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+    }
+   
+    updateClock();
+    let timeinterval = setInterval(updateClock, 1000);
+  }
+   
+  let deadline = new Date(Date.parse(new Date()) + 5 * 1000);
+  initializeClock("countdown", deadline);
+
+  if (t.total <= 0) {
+    clearInterval(timeinterval);
+    let deadline = new Date(Date.parse(new Date()) + 5 * 1000);
+    initializeClock('countdown', deadline);
+  }
+
 */
-/*
-let more_but = document.querySelector('#more');
-
-more_but.addEventListener("click", moreExe);
-*
-
-/*
+/* КОНСПЕКТ
 let plus = ["+"];                             // сделали элементом массива      
 let minus = ["-"];
 let division = ["/"];
@@ -104,7 +136,14 @@ function count(i) {
 }
 
 */
+//ИВЕНТ
+/*
+document.getElementById("more").addEventListener("click", newTask);
 
+function newTask() {
+    w.value = '';
+}
+*/
 
 /* функция считывания инпута
 function asd(value) {                         //ивент onchange в html
@@ -118,6 +157,40 @@ function asd(value) {                         //ивент onchange в html
 }
 */
 
+/* РАБОТАЮЩЕЕ С ОНЧЭНДЖ В html и через //const a = document.getElementsByTagName("userInput");
 
+function count(value) {  
+    let g = value;
+    if(g == optionS || g == optionM || g == optionMul || g == optionD) {
+        result.innerHTML = 'Well done!';
+    } else {
+        result.innerHTML = 'Try again!';
+        //w.value = '';                    //через айди.value очищается поле ввода
+    }  
+}
+*/
+/*
+//РАБОТАЕТ ТАЙТЕР ОТДЕЛЬНЫЙ
+    
+let limit = mathCalculator("limit")||5;
+limit = parseInt(limit);
 
+processTimer();
 
+function processTimer() {
+  if (limit > 0) {
+    setTimeout("processTimer()", 1000);
+    limit--;
+  } else {
+    timer.style.color = `rgb(255, 0, 0)`;
+    timer.style.fontSize = '25px';
+  }
+  let limit_div = ("0"+parseInt(limit / 60)).slice(-2); // минуты
+  let limit_mod = ("0"+(limit - limit_div * 60)).slice(-2); // секунды
+  // вывод времени
+  el_timer = document.getElementById("timer");
+  if (el_timer) el_timer.innerHTML = limit_div+":"+limit_mod;
+}
+
+setInterval ( processTimer, 5000 );
+*/
--- mini_tasks/math_calculator/math_calculator.js
+++ mini_tasks/math_calculator/math_calculator.js
@@ -1,3 +1,158 @@
+let mathCalculator = function() {
+    const A = randNumber();
+    const B = randNumber();
+    const C = randOperand();
+    const optionS = A + B;
+    const optionM = A - B;
+    const optionMul = A * B;
+    const optionD = A / B;
+    
+    function randNumber(min = 10, max = 100) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    
+    function randOperand(min = 1, max = 4) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    
+    const start = function() {
+        if(C == 1){
+            given.innerHTML = `${A} + ${B}`;
+        } else if(C == 2){
+            given.innerHTML = `${A} - ${B}`;
+        } else if(C == 3){
+            given.innerHTML = `${A} * ${B}`;
+        } else if(C == 4){
+            given.innerHTML = `${A} / ${B}`;
+        }
+    }
+    
+    start();
+    
+    function count() {  
+        g = w.value;
+        if(g == optionS || g == optionM || g == optionMul || g == optionD) {
+            result.innerHTML = 'Well done!';
+        } else {
+            result.innerHTML = 'Try again!';
+            //w.value = '';                    //через айди.value очищается поле ввода
+        }  
+    }
+    
+    submit.onclick = count;
+    
+    w.value = '';
+    
+    }
+    
+    more.onclick = mathCalculator;
+    setInterval ( mathCalculator, 5000 );
+    
+    
+    let limit = mathCalculator("limit")||5;
+    limit = parseInt(limit);
+    
+    processTimer();
+    
+    function processTimer() {
+      if (limit > 0) {
+        setTimeout("processTimer()", 1000);
+        limit--;
+      } else {
+        timer.style.color = `rgb(255, 0, 0)`;
+        timer.style.fontSize = '25px';
+      }
+      let limit_div = ("0"+parseInt(limit / 60)).slice(-2); // минуты
+      let limit_mod = ("0"+(limit - limit_div * 60)).slice(-2); // секунды
+      // вывод времени
+      el_timer = document.getElementById("timer");
+      if (el_timer) el_timer.innerHTML = limit_div+":"+limit_mod;
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+/* КАЛЬКУЛЯТОР VICE VERSE
+html
+<main>
+        <div class="identification">#</div>
+        <div class="in">IN</div>
+        <div class="out">OUT</div>
+        <div class="index_number"></div>
+        <div class="set_number"></div>
+        <div class="user_input">
+            <input type="text" class="user_enter_input" placeholder="3 * 4 =" onchange="a_value(this);"/>
+            <span class="assessment"></span>
+        </div>
+    </main>
+
+css
+body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    justify-items: center;
+    align-items: center;
+    overflow: hidden;
+    font-family: Arial, Helvetica, sans-serif;
+}
+main {
+    display: grid;
+    grid-template: 30% 70% / 10% 35% 55%;
+    width: 350px;
+    height: 300px;
+}
+.identification, .in, .out, .index_number, .set_number, .user_input {
+    padding-top: 20px;
+    padding-left: 12px;
+    border: 1px solid green;
+}
+.identification {
+    grid-area: 1 / 1;
+    border-bottom: 1px solid transparent;
+}
+.in {
+    grid-area: 1 / 2;
+    border-left: 1px solid transparent;
+    border-right: 1px solid transparent;
+    border-bottom: 1px solid transparent;
+}
+.out {
+    grid-area: 1 / 3;;
+    border-bottom: 1px solid transparent;
+}
+.index_number {
+    grid-area: 2 / 1;
+}
+.set_number {
+    grid-area: 2 / 2;
+    border-left: 1px solid transparent;
+    border-right: 1px solid transparent;
+}
+.user_input {
+    grid-area: 2 / 3;
+}
+.user_enter_input {
+    border: 1px solid transparent;
+    outline: none;
+}
+.assessment {
+    position: relative;
+    top: 50px;
+}
+
+js
+
 function randNumber(min = 0, max = 100) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
